7812af1fbf14192803581caf64d642f4ae7daba1
zhangjtroger <zhangjtroger@gmail.com>
2022-06-25 10:53:08 +0800

    1

--- xlsx2docx/test.py
+++ xlsx2docx/test.py
@@ -1,28 +1,19 @@
-import pandas as pd
-import docx
+from docxtpl import DocxTemplate
+from openpyxl import load_workbook,Workbook
 
+filename = ''
 
-def search(word):
-    doc = docx.Document("./gork.docx")
-    all_paragraphs = doc.paragraphs
-    list1 = []
-    for paragraph in doc.paragraphs:
-        str1 = paragraph.text
-        if str1.find(word) != -1:
-            list1.append(str1)
-    if list1 == []:
-        list1.append("failed,not found")
-    return list1
-
-
-def replace():
-    data_src = pd.read_excel(io=r"./gork.xlsx", nrows=1)
-    for i in data_src:
-        ser_word = "{%s}" % i
-        if search(ser_word) == ["failed,not found"]:
-            print("抱歉，没有在表格中找到要求替换的数据，请检查格式是否正确")
-
-
-if __name__ == "__main__":
-    replace()
-    
+doc = DocxTemplate('gork.docx')
+wb = load_workbook('gork.xlsx', data_only = True)
+sheet = wb.active
+for row in sheet:
+	rowMap = map(lambda x:x.value, row)
+	if row[0].row ==  1:
+		title = list(rowMap)
+	else:
+		context = dict(zip(title, rowMap))
+		filename = context['filename']
+		print('Rendering', filename, '...', end = '\r' )
+		doc.render(context)
+		doc.save(filename)
+		print('Rendering', filename, 'done.')
